45b5b744cb0154561cd42dc3ee3b149342f92d27
Kurt Stolle <k.h.w.stolle@gmail.com>
2020-05-03 13:09:12 +0200

    +

--- paper/data/notes.jl
+++ paper/data/notes.jl
@@ -2,7 +2,6 @@ using Plots
 using StatsPlots
 using CSV
 
-## Plot baseline results
 epochs = 10
 
 function plot_train_val(batch_size, samples,fname_train, fname_val)
@@ -20,33 +19,33 @@ function plot_train_val(batch_size, samples,fname_train, fname_val)
 
     xlabel!("Epoch")
     ylabel!("Value")
-    
+
     xticks!(0:1:epochs)
 end
 
-plot_train_val(2,2975,"baseline/train_loss_lr0-01.csv","baseline/val_loss_lr0-01.csv")
+plot_train_val(2,2975,"data/baseline/train_loss_lr0-01.csv","data/baseline/val_loss_lr0-01.csv")
 
-plot_train_val(2,2975,"augment/train_loss_hflip.csv", "augment/val_loss_hflip.csv")
+plot_train_val(2,2975,"data/augment/train_loss_hflip.csv", "data/augment/val_loss_hflip.csv")
 
-plot_train_val(2,2975,"threshold/train_loss.csv", "threshold/val_loss.csv")
-plot_train_val(2,2975,"threshold/train_iou.csv", "threshold/val_iou.csv")
+plot_train_val(2,2975,"data/threshold/train_loss.csv", "data/threshold/val_loss.csv")
+plot_train_val(2,2975,"data/threshold/train_iou.csv", "data/threshold/val_iou.csv")
 
-plot_train_val(2,2975,"reduced-dims/train_loss.csv", "reduced-dims/val_loss.csv")
-plot_train_val(2,2975,"reduced-dims/train_iou.csv", "reduced-dims/val_iou.csv")
+plot_train_val(2,2975,"data/reduced-dims/train_loss.csv", "data/reduced-dims/val_loss.csv")
+plot_train_val(2,2975,"data/reduced-dims/train_iou.csv", "data/reduced-dims/val_iou.csv")
 
-plot_train_val(2,2975,"dropout/train_loss.csv", "dropout/val_loss.csv")
-plot_train_val(2,2975,"dropout/train_iou.csv", "dropout/val_iou.csv")
+plot_train_val(2,2975,"data/dropout/train_loss.csv", "data/dropout/val_loss.csv")
+plot_train_val(2,2975,"data/dropout/train_iou.csv", "data/dropout/val_iou.csv")
 
-plot_train_val(2,2975,"iou-loss/train_loss.csv", "iou-loss/val_loss.csv")
-plot_train_val(2,2975,"iou-loss/train_iou.csv", "iou-loss/val_iou.csv")
+plot_train_val(2,2975,"data/iou-loss/train_loss.csv", "data/iou-loss/val_loss.csv")
+plot_train_val(2,2975,"data/iou-loss/train_iou.csv", "data/iou-loss/val_iou.csv")
 
-plot_train_val(2,2975,"alpha-edges/train_loss.csv", "alpha-edges/val_loss.csv")
-plot_train_val(2,2975,"alpha-edges/train_iou.csv", "alpha-edges/val_iou.csv")
+plot_train_val(2,2975,"data/alpha-edges/train_loss.csv", "data/alpha-edges/val_loss.csv")
+plot_train_val(2,2975,"data/alpha-edges/train_iou.csv", "data/alpha-edges/val_iou.csv")
 
-plot_train_val(2,2975,"bilinear-false/train_loss.csv", "bilinear-false/val_loss.csv")
-plot_train_val(2,2975,"bilinear-false/train_iou.csv", "bilinear-false/val_iou.csv")
+plot_train_val(2,2975,"data/bilinear-false/train_loss.csv", "data/bilinear-false/val_loss.csv")
+plot_train_val(2,2975,"data/bilinear-false/train_iou.csv", "data/bilinear-false/val_iou.csv")
 
-plot_train_val(2,2975,"optimized-lr/0-001/train_loss.csv", "optimized-lr/0-001/val_loss.csv")
-plot_train_val(2,2975,"optimized-lr/0-001/train_iou.csv", "optimized-lr/0-001/val_iou.csv")
-plot_train_val(2,2975,"optimized-lr/0-01/train_loss.csv", "optimized-lr/0-01/val_loss.csv")
-plot_train_val(2,2975,"optimized-lr/0-01/train_iou.csv", "optimized-lr/0-01/val_iou.csv")
+plot_train_val(2,2975,"data/optimized-lr/0-001/train_loss.csv", "data/optimized-lr/0-001/val_loss.csv")
+plot_train_val(2,2975,"data/optimized-lr/0-001/train_iou.csv", "data/optimized-lr/0-001/val_iou.csv")
+plot_train_val(2,2975,"data/optimized-lr/0-01/train_loss.csv", "data/optimized-lr/0-01/val_loss.csv")
+plot_train_val(2,2975,"data/optimized-lr/0-01/train_iou.csv", "data/optimized-lr/0-01/val_iou.csv")
